76ae7844a1262ecb595ed558e9006816802881be
Taotao Hsu <taotaohsu@gmail.com>
2019-04-29 10:29:00 +0800

    feat: add archive and security support

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+ARCHIVE_LIST=(wheezy)
+
 ARCH=$1
 DISTRO=$2
 BUILD_DIR=/root/${ARCH}_${DISTRO}
 
 DEB_URL="http://deb.debian.org/debian/"
+DEB_SECURITY_URL="http://deb.debian.org/debian-security/"
+ARCHIVE_URL="http://archive.debian.org/debian/"
+ARCHIVE_SECURITY_URL="http://archive.debian.org/debian-security"
 
 export DEBIAN_FRONTEND="noninteractive"
 export DEBCONF_NONINTERACTIVE_SEEN="true"
@@ -20,12 +25,20 @@ service binfmt-support restart
 
 mkdir -p "${BUILD_DIR}"
 
+URL="${DEB_URL}"
+SECURITY_URL="${DEB_SECURITY_URL}"
+if [[ ${ARCHIVE_LIST[*]} =~ ${DISTRO} ]]; then
+	URL="${ARCHIVE_URL}"
+	SECURITY_URL="${ARCHIVE_SECURITY_URL}"
+fi
+
 LC_ALL=C LANGUAGE=C LANG=C \
 	qemu-debootstrap \
 	--variant=minbase \
 	--arch "${ARCH}" \
-	"${DISTRO}" "${BUILD_DIR}" "${DEB_URL}"
+	"${DISTRO}" "${BUILD_DIR}" "${URL}"
 
-echo "deb ${DEB_URL} ${DISTRO} main contrib non-free" > "${BUILD_DIR}/etc/apt/sources.list"
+echo "deb ${URL} ${DISTRO} main contrib non-free" > "${BUILD_DIR}/etc/apt/sources.list"
+echo "deb ${SECURITY_URL} ${DISTRO} main contrib non-free" >> "${BUILD_DIR}/etc/apt/sources.list"
 
 tar -C "${BUILD_DIR}"  -f /root/build/image.tar -c .
